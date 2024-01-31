0f5bce036eff7cc377324768769ffc915a393c69
George Michael Pussl <george.pussl@gmail.com>
2024-01-31 04:32:48 +0300

    0x06-regular_expressions

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,18 +1,2 @@
 #!/usr/bin/env ruby
-
-pattern = /ht{2,5}n/
-
-# Check if there is an argument provided
-if ARGV.length != 1
-  puts "Usage: #{$PROGRAM_NAME} <string>"
-  exit 1
-end
-
-# Get the argument
-string = ARGV[0]
-
-# Match the pattern in the argument
-match = string.scan(pattern)
-
-# Output the result
-puts match.join
+puts ARGV[0].scan(/hbt*n/).join
